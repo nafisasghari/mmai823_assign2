203ab3b5fca13847315578ce624efcaa0a2456fe
Colin Roos <colin.a.roos@gmail.com>
2020-01-14 22:33:37 -0500

    Finalize model hyperparameters.

--- a2_main.py
+++ a2_main.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import datetime
+import numpy as np
 from imblearn.ensemble import BalancedRandomForestClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import balanced_accuracy_score, classification_report, roc_auc_score
 from yellowbrick.model_selection import ValidationCurve
-from yellowbrick.classifier import ROCAUC
+from yellowbrick.classifier import ROCAUC, ConfusionMatrix
 from util_helper import resample_to_csv
 
 
@@ -34,12 +35,30 @@ Xr_train = dfr.drop('BK', axis=1)
 # Xr_train, Xr_test, yr_train, yr_test = train_test_split(Xr, yr, test_size=0.2, random_state=randomState)
 
 # Benchmark using regular RFC
-# rf = RandomForestClassifier(n_estimators=50, random_state=randomState)
-# rf.fit(Xr_train, yr_train)
+rfr = RandomForestClassifier(n_estimators=50, max_depth=5, random_state=randomState)
+rfr.fit(Xr_train, yr_train)
 
 # Using Resampled
-rfr = RandomForestClassifier(n_estimators=50, random_state=randomState)
-rfr.fit(Xr_train, yr_train)
+estimators = np.arange(30, 60, 2)
+depths = np.arange(2, 6, 1)
+scores = []
+
+# for est in estimators:
+#     for dp in depths:
+#         rfr = RandomForestClassifier(n_estimators=est, max_depth=dp, random_state=randomState)
+#         rfr.fit(Xr_train, yr_train)
+#         y_pred = rfr.predict(X_test)
+#         rf_score = balanced_accuracy_score(y_test, y_pred)
+#         print('Estimators: ' + str(est))
+#         print('Depth: ' + str(dp))
+#         print('AUC Score: ' + str(rf_score) + '\n')
+#         scores.append(rf_score)
+#
+# df_scores = pd.DataFrame(columns=['n_estimators', 'max_depth', 'auc_score'])
+# df_scores['n_estimators'] = estimators
+# df_scores['max_depth'] = depths
+# df_scores['auc_score'] = scores
+
 
 # Predictions
 y_pred = rfr.predict(X_test)
@@ -61,57 +80,20 @@ print('Resampled Balanced Accuracy: ' + str(rf_score))
 print('Resampled AUC Score:' + str(rf_auc))
 print(classification_report(y_test, y_pred, digits=3))
 
-# cm1 = ConfusionMatrix(rf, classes=['Not Bankrupt', 'Bankrupt'])
+cm1 = ConfusionMatrix(rfr, classes=['Not Bankrupt', 'Bankrupt'])
 # roc1 = ROCAUC(rf, classes=['Not Bankrupt', 'Bankrupt'])
 # roc2 = ROCAUC(rfr, classes=['Not Bankrupt', 'Bankrupt'])
-# # cm1.score(X_test, y_test)
+cm1.score(X_test, y_test)
 # roc1.score(X_test, y_test)
 # roc2.score(X_test, y_test)
-# # cm1.show()
+cm1.show()
 # roc1.show()
 # roc2.show()
 
-# # Use a balanced RFC
-# brf = BalancedRandomForestClassifier(n_estimators=50, random_state=randomState)
-# brf.fit(X_train, y_train)
-#
-# y_pred = brf.predict(X_test)
-# brf_score = balanced_accuracy_score(y_test, y_pred)
-# print('Balanced Accuracy: ' + str(brf_score))
-# print(classification_report(y_test, y_pred, digits=2))
-# cm2 = ConfusionMatrix(brf, classes=['Not Bankrupt', 'Bankrupt'])
-# # cm2.score(X_test, y_test)
-# # cm2.show()
-#
-# # Try resampling
-# smote_enn = SMOTEENN(random_state=randomState)
-# X_resampled, y_resampled = smote_enn.fit_resample(X, y)
-# X_trainr, X_testr, y_trainr, y_testr = train_test_split(X_resampled, y_resampled, test_size=0.2, random_state=randomState)
-#
-# brf2 = RandomForestClassifier(n_estimators=50, random_state=randomState)
-# brf2.fit(X_trainr, y_trainr)
-#
-# y_pred2 = brf2.predict(X_testr)
-# brf_score2 = balanced_accuracy_score(y_testr, y_pred2)
-# print('reBalanced Accuracy: ' + str(brf_score2))
-# print(classification_report(y_testr, y_pred2, digits=2))
-# cm3 = ConfusionMatrix(brf2, classes=['Not Bankrupt', 'Bankrupt'])
-# roc3 = ROCAUC(brf2, classes=['Not Bankrupt', 'Bankrupt'])
-# cm3.score(X_testr, y_testr)
-# roc3.score(X_testr, y_testr)
-# cm3.show()
-# roc3.show()
-
 # Generate a validation curve to find the efficiency point
-# viz_rf = ValidationCurve(rf, param_name='n_estimators',
-#                          param_range=np.arange(25, 150, 25), cv=4, scoring='f1_weighted')
+# rf = RandomForestClassifier(n_estimators=12, random_state=randomState)
+# viz_rf = ValidationCurve(rf, param_name='max_depth',
+#                          param_range=np.arange(8, 20, 1), cv=4, scoring='f1_weighted')
 #
-# viz_rf.fit(X, y)
-#
-# viz_brf = ValidationCurve(brf, param_name='n_estimators',
-#                           param_range=np.arange(25, 150, 25), cv=4, scoring='f1_weighted')
-#
-# viz_brf.fit(X, y)
-#
+# viz_rf.fit(Xr_train, yr_train)
 # viz_rf.show()
-# viz_brf.show()
